52ba0c5d3d275913ff50f11e8f181fb545f042f1
zmx <zmx0142857@qq.com>
2018-01-26 20:24:18 +0800

    edit function.py:     fixed small bugs

--- function.py
+++ function.py
@@ -17,8 +17,7 @@ class FuncType(object):
         return self._name == other._name and self._flags == other._flags
 
     def __call__(self, *args):
-        if len(args) >= 1 and all(isinstance(arg, Function) for arg in args):
-            return Function(self, *args)
+        return Function(self, *args)
 
     """ ABOUT 'FLAGS'
             argument-count  precedence  associativity   fix-style
@@ -38,44 +37,46 @@ def Const(val):
 def Identity(s):
     return FuncType(s, None, 0x138)
 
-NEG = FuncType('-', lambda x: -x, 0x58)         # 001011000, 0130
-ABS = FuncType('| |', abs, 0x7b)                # 001111011, 0173
-ceil = FuncType('|~ ~|', math.ceil, 0x7b)
-floor = FuncType('|_ _|', math.floor, 0x7b)
-sin = FuncType('sin', math.sin, 0x68)           # 001101000, 0150
-cos = FuncType('cos', math.cos, 0x68)
-tan = FuncType('tan', math.tan, 0x68)
-sinh = FuncType('sinh', math.sinh, 0x68)
-cosh = FuncType('cosh', math.cosh, 0x68)
-tanh = FuncType('tanh', math.tanh, 0x68)
-asin = FuncType('arcsin', math.asin, 0x68)
-acos = FuncType('arccos', math.acos, 0x68)
-atan = FuncType('arctan', math.atan, 0x68)
-asinh = FuncType('arcsinh', math.asinh, 0x68)
-acosh = FuncType('arccosh', math.acosh, 0x68)
-atanh = FuncType('arctanh', math.atanh, 0x68)
-# FAC = FuncType('!', math.factory, 0x72)         # 001110010, 0162
-ADD = FuncType(' + ', lambda x, y: x+y, 0x89)   # 010001001, 0211
-SUB = FuncType(' - ', lambda x, y: x-y, 0x89)
-MUL = FuncType(' * ', lambda x, y: x*y, 0x91)   # 010010001, 0221
-DIV = FuncType(' / ', lambda x, y: x/y, 0x91)
-LOG = FuncType('log', math.log, 0xa0)           # 010100000, 0240
-POW = FuncType('^', lambda x, y: x**y, 0xb5)    # 010110101, 0265
+NEG   = FuncType('-',     lambda x: -x,              0x58) # 001011000, 0130
+ABS   = FuncType('| |',   abs,                       0x7b) # 001111011, 0173
+ceil  = FuncType('|~ ~|', math.ceil,                 0x7b)
+floor = FuncType('|_ _|', math.floor,                0x7b)
+sin   = FuncType('sin',   math.sin,                  0x68) # 001101000, 0150
+cos   = FuncType('cos',   math.cos,                  0x68)
+tan   = FuncType('tan',   math.tan,                  0x68)
+sinh  = FuncType('sinh',  math.sinh,                 0x68)
+cosh  = FuncType('cosh',  math.cosh,                 0x68)
+tanh  = FuncType('tanh',  math.tanh,                 0x68)
+asin  = FuncType('asin',  math.asin,                 0x68)
+acos  = FuncType('acos',  math.acos,                 0x68)
+atan  = FuncType('atan',  math.atan,                 0x68)
+asinh = FuncType('asinh', math.asinh,                0x68)
+acosh = FuncType('acosh', math.acosh,                0x68)
+atanh = FuncType('atanh', math.atanh,                0x68)
+fac   = FuncType('!',     lambda n: math.gamma(n+1), 0x72) # 001110010, 0162
+ADD   = FuncType(' + ',   lambda x, y: x+y,          0x89) # 010001001, 0211
+SUB   = FuncType(' - ',   lambda x, y: x-y,          0x89)
+MUL   = FuncType(' * ',   lambda x, y: x*y,          0x91) # 010010001, 0221
+DIV   = FuncType(' / ',   lambda x, y: x/y,          0x91)
+LOG   = FuncType('log',   math.log,                  0xa0) # 010100000, 0240
+POW   = FuncType('^',     lambda x, y: x**y,         0xb5) # 010110101, 0265
 
 class Function(object):
     """
     Doctest:
-    >>> c = Function(3.14)
-    >>> c
+    >>> Function(3.14)
     3.14
-    >>> x, y = var('x', 'y')
-    >>> x
+    >>> var()
     x
+    >>> var('v')
+    v
+    >>> x, y = var('x', 'y')
+    >>> sin(x)
+    sin x
     >>> x(42)
     42
-    >>> u = atan(x + abs(1+x**2))
-    >>> u
-    arctan(x + |1 + x^2|)
+    >>> atan(x + abs(1+x**2))
+    atan(x + |1 + x^2|)
     >>> z = x**2 + 3 * y + x*y
     >>> z(0.5, 2)
     7.25
@@ -126,6 +127,9 @@ class Function(object):
         def put(content, condition):
             return '(' + str(content) + ')' if condition else str(content)
 
+        def pad(condition):
+            return ' ' if condition else ''
+
         if self.is_identity():
             return self.name()
         elif self.is_const():
@@ -140,7 +144,7 @@ class Function(object):
                     )
         
             if self.fix() == 0: # pre-fix
-                return self.name() + put(f1, cond)
+                return self.name() + pad(self.name().isalpha() and not cond) + put(f1, cond)
             elif self.fix() == 2: # post-fix
                 return put(f1, cond) + self.name()
             elif self.fix() == 3: # both-side
@@ -149,7 +153,7 @@ class Function(object):
                 raise ValueError("unary function '%s' has infix property!"
                         % self.name())
 
-        else: # self.argc() == 2:
+        elif self.argc() == 2:
             f2 = self._funcs[1]
             cond1 = f1.prec() <= self.prec()
             cond2 = (f2.is_const() and f2() < 0)\
@@ -157,13 +161,15 @@ class Function(object):
                     or f2.prec() <= self.prec()
 
             if self.fix() == 0: # pre-fix
-                return self.name() + '(' + put(f1, cond1) + ' '\
+                return self.name() + '(' + put(f1, cond1) + ', '\
                         + put(f2, cond2) + ')'
             elif self.fix() == 1: # in-fix
                 return put(f1, cond1) + self.name() + put(f2, cond2)
             else: # post-fix
-                return '(' + put(f1, cond1) + ' ' + put(f2, cond2) + ')'\
+                return '(' + put(f1, cond1) + ', ' + put(f2, cond2) + ')'\
                         + self.name()
+        else:
+            return self.name() + '(' + ', '.join(str(self._funcs)) + ')'
 
     __repr__ = __str__
 
@@ -210,35 +216,38 @@ class Function(object):
         return ADD(self, other)
 
     def __radd__(self, other):
-        return Function(ADD, other, self)
+        return ADD(other, self)
 
     def __sub__(self, other):
-        return Function(SUB, self, other)
+        return SUB(self, other)
 
     def __rsub__(self, other):
-        return Function(SUB, other, self)
+        return SUB(other, self)
 
     def __mul__(self, other):
-        return Function(MUL, self, other)
+        return MUL(self, other)
 
     def __rmul__(self, other):
-        return Function(MUL, other, self)
+        return MUL(other, self)
 
     def __truediv__(self, other):
-        return Function(DIV, self, other)
+        return DIV(self, other)
 
     def __rtruediv__(self, other):
-        return Function(DIV, other, self)
+        return DIV(other, self)
 
     def __pow__(self, other):
-        return Function(POW, self, other)
+        return POW(self, other)
 
     def __rpow__(self, other):
-        return Function(POW, other, self)
+        return POW(other, self)
 
 def var(*args):
     if all(isinstance(s, str) for s in args):
-        return tuple(Function(s) for s in args)
+        if len(args) < 2:
+            return Function(*args)
+        else:
+            return tuple(Function(s) for s in args)
 
 if __name__ == '__main__':
     import doctest
