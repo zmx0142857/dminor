4a49c9eb3c5655bc5926f6954d006a39d88ece35
zmx <zmx0142857@qq.com>
2018-03-23 22:35:52 +0800

    create pi.html edit pi.py ln css js mathjax .

--- pi.py
+++ pi.py
@@ -7,10 +7,10 @@ __author__ = 'Clarence Zhuo'
 
 import math, rational
 
-def pi(n):
+def my_first_pi(n):
+    """performs well for n <= 100. bigger n costs too much time."""
     ret = 0
-    for i in range(int(n / math.log10(2.25))):
+    N = int(n / math.log10(2.25))
+    for i in range(N):
         ret += rational.Rat(rational.choose(2*i, i)*3, (16**i * (2*i+1)))
     return ret.to_float(n)
-
-print(pi(100))
